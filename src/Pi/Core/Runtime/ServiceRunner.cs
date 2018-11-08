377469d1cc71383486d3811257baba259ee51ac8
Niklas Hedlund <nojan1989@gmail.com>
2018-11-08 15:58:47 +0100

    Added montoring task and panic support

--- src/Pi/Core/Runtime/ServiceRunner.cs
+++ src/Pi/Core/Runtime/ServiceRunner.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using Core.Services;
+using Core.Services.Models;
 
 namespace Core.Runtime
 {
     public class ServiceRunner
     {
         private CancellationTokenSource _cancellationTokenSource;
-        private List<Task> _tasks;
+        private Task[] _tasks;
+        private Task _monitoringTask;
         private IComponentContext _context;
 
         public ServiceRunner(IComponentContext context)
@@ -24,18 +27,59 @@ namespace Core.Runtime
                 return;
 
             _cancellationTokenSource = new CancellationTokenSource();
-            _tasks = new List<Task>();
+            var taskList = new List<Task>();
 
             foreach (var service in _context.Resolve<IEnumerable<BaseService>>())
             {
-                _tasks.Add(service.Run(_cancellationTokenSource.Token));
+                taskList.Add(service.Run(_cancellationTokenSource.Token));
             }
+
+            _tasks = taskList.ToArray();
+            _monitoringTask = Task.Run(() => MonitoringTask());
         }
 
         public void Stop()
         {
             _cancellationTokenSource.Cancel();
-            Task.WaitAll(_tasks.ToArray());
+            Task.WaitAll(_tasks);
+            _monitoringTask.Wait();
+        }
+
+        private void MonitoringTask()
+        {
+            while (!_cancellationTokenSource.IsCancellationRequested)
+            {
+                var faultedTask = _tasks.FirstOrDefault(t => t.IsFaulted);
+                if (faultedTask != null)
+                {
+                    //Panic!
+                    //TODO: Log
+                    Console.WriteLine($"ERROR! Service task {faultedTask.Id} has faulted. Terminating all services");
+
+                    _cancellationTokenSource.Cancel();
+
+                    foreach (var panicService in _context.Resolve<IEnumerable<IPanic>>())
+                    {
+                        try
+                        {
+                            panicService.OnPanic();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Exception in panic handler ({nameof(panicService)}): {e.Message}");
+                        }
+                    }
+
+                    return;
+                }
+
+                Task.Delay(50).Wait();
+            }
+        }
+
+        private void StopServiceTasks()
+        {
+
         }
     }
 }
---
+++ src/Pi/Core/Services/Models/IPanic.cs
@@ -0,0 +1,7 @@
+namespace Core.Services.Models
+{
+    public interface IPanic
+    {
+         void OnPanic();
+    }
+}
